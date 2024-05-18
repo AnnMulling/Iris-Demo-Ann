35b9cc88d4aff381b2610eaf7c5a9b97f341869e
AnnPrim <Primpraowann@gmail.com>
2024-05-17 21:26:52 -0500

    Initial Commit

---
+++ Iris-Demo/Iris_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Iris_DemoApp.swift
+//  Iris-Demo
+//
+//  Created by Primpraow Mulling on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Iris_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
