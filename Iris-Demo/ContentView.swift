4e624896504af6060ee46ab653a4a5cc70f094e9
AnnPrim <Primpraowann@gmail.com>
2024-05-19 23:27:14 -0500

    first commit

--- Iris-Demo/ContentView.swift
+++ Iris-Demo/ContentView.swift
@@ -8,15 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let costumes = ["image 4",
+                            "image 5",
+                            "image 8",
+                            "image 10",
+                            "image 13"]
+    
     var body: some View {
+      
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+            /* navigation bar*/
+            HStack(alignment: .center) {
+               Image(systemName: "line.horizontal.3")
+                   .foregroundColor(.purple)
+                   .imageScale(.large)
+               Text("Home")
+                   .foregroundColor(.purple)
+               Spacer()
+               Image(systemName: "gearshape")
+                   .foregroundColor(.purple)
+                   .imageScale(.large)
+                   .foregroundStyle(.tint)
+               
+            }
+            
+            /* carousel */
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(costumes, id: \.self) { costume in
+                        
+                        Image(costume)
+                            
+                            .resizable()
+                            .scaledToFill()
+                            .frame(height: 100)
+                          
+                            .background(Color(.white))
+                            .padding(.horizontal, 20)
+                        
+                    }
+                }
+            }
+           
+       }
+        .padding(.all)
+               
+           }
+        
 }
 
 #Preview {
